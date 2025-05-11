8ea7f7f17733dd0d474d9b25dccb4816fa75e090
unknown <eric.gomes205@gmail.com>
2025-05-11 20:40:48 -0300

    correção

--- src/components/curriculo.tsx
+++ src/components/curriculo.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
 const DownloadCurriculo: React.FC = () => {
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = '../../public/Curriculo.pdf';
+    link.download = 'Curriculo.pdf';
+    link.click();
+  };
   return (
     <div className="p-4  bg-gray-100 dark:bg-gray-800 flex justify-center items-center rounded-lg">
-      <a
-        href="../../public/Curriculo.pdf"
-        download="Curriculo.pdf"
-        className="p-4 bg-gray-00  rounded-lg hover:bg-blue-600 "
+      <button
+        className="p-4 bg-gray-00  rounded-lg hover:bg-blue-600 rounded-sm"
+        onClick={handleDownload}
       >
         Baixar Currículo
-      </a>
+      </button>
     </div>
   );
 };
